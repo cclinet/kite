0de590ce8e0c5815a1ada6bb66687efdcab3f2ba
cclin <cclinet@outlook.com>
2024-01-31 15:44:54 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import "./style.css";
-import { mdProcess } from "./mdProcess.ts";
 import menuHtml from './menu.html?raw'
+import "highlight.js/styles/github.css";
 // import "mathjax/es5/tex-chtml";
 // import "mathjax/es5/output/chtml/fonts/woff-v2"
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeHighlight from "rehype-highlight";
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax/browser";
+import rehypeStringify from "rehype-stringify";
+async function mdProcess(text:string) {
+  const processor = unified()
+  .use(remarkParse)
+  .use(remarkMath)
+  .use(remarkRehype)
+  .use(rehypeMathjax)
+  .use(rehypeHighlight)
+  .use(rehypeStringify);
+  return (await processor.process(text)).toString()
+}
 
 const response = await fetch("config.json");
 const config: { title: string; path: string }[] = await response.json();
@@ -23,11 +40,10 @@ if (pathname === "/") {
     menu.appendChild(li);
   });
 } else {
-
-  const response = await fetch(pathname + ".md");
-  const text = await response.text();
-  console.log(text);
-  const htmlContent = await mdProcess(text);
+  const {default: file} = await import("./posts/ss.md?raw");
+  console.log(file)
+  const htmlContent = await mdProcess(file);
+  console.log(htmlContent)
   appContainer.innerHTML = htmlContent;
   // @ts-ignore
   // await MathJax.typesetPromise();
