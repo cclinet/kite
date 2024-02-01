b00453c4edd3fae7202ac341ad9c0985ec338333
cclin <cclinet@outlook.com>
2024-02-01 11:06:25 +0800

    update

--- plugin/compileMDToHTML.ts
+++ plugin/compileMDToHTML.ts
@@ -3,7 +3,7 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeHighlight from "rehype-highlight";
 import remarkMath from "remark-math";
-import rehypeMathjax from "rehype-mathjax/chtml";
+import rehypeMathjax from "rehype-mathjax/browser";
 import rehypeStringify from "rehype-stringify";
 
 export default async function myPlugin() {
@@ -12,9 +12,13 @@ export default async function myPlugin() {
     .use(remarkMath)
     .use(remarkRehype)
     .use(rehypeMathjax, {
-      chtml: {
-        fontURL:
-          "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",
+      tex: {
+        inlineMath: [
+          ["$", "$"],
+        ],
+        displayMath: [
+          ["$$", "$$"],
+        ],
       },
     })
     .use(rehypeHighlight)
@@ -28,10 +32,10 @@ export default async function myPlugin() {
       if (fileRegex.test(id)) {
         const source = code.slice('export default "'.length, -1);
 
-        console.log(source.replace(/\\n/g, "\n"));
+        console.log(source);
         const result = await processor.process(source.replace(/\\n/g, "\n"));
-        const resultString = result.toString().replace(/\n/g, "\\n");
-
+        const resultString = result.toString().replace(/\\/g, "$&");
+        console.log(resultString);
         return {
           code: `export default \`${resultString}\``,
         };
