8b1d715a7806d1a6e929045fec6dcba85fc5bd1c
cclin <cclinet@outlook.com>
2024-03-25 15:06:29 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cclin.org",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { output: "mathml" }]],
+  },
 });
