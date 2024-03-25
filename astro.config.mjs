61c198dbf939bdbf36aa43d1ff294f950c2c7a32
cclin <cclinet@outlook.com>
2024-03-25 15:38:46 +0800

    update path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cclin.org",
+  site: "https://blog.cclin.org",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
